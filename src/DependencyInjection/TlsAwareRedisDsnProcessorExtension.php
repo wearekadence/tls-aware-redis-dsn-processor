e457f70b7dadb90a9a59f3d3d920f9e4caaa68e7
Stuart Taylor <stuart@chargifi.com>
2023-01-09 12:19:03 +0000

    Add services config loader

---
+++ src/DependencyInjection/TlsAwareRedisDsnProcessorExtension.php
@@ -0,0 +1,16 @@
+<?php
+namespace WeAreKadence\TlsAwareRedisDsnProcessor\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class TlsAwareRedisDsnProcessorExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
+        $loader->load('services.yaml');
+    }
+}
