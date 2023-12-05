1b916ecc41c84bf1f097e85c4be62df89b702f1d
Jordan de Laune <jordan@chargifi.com>
2023-12-05 14:59:44 +0000

    Add return type

--- src/DependencyInjection/TlsAwareRedisDsnProcessorExtension.php
+++ src/DependencyInjection/TlsAwareRedisDsnProcessorExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class TlsAwareRedisDsnProcessorExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
